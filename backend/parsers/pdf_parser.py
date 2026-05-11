6c4c964d73647d1bb3d9e4f2ccfdbf1c08438be7
Binod Dey <biniddey06@gmail.com>
2026-05-11 22:55:40 +0530

    feat: Parses text from PDF filetype

---
+++ backend/parsers/pdf_parser.py
@@ -0,0 +1,16 @@
+import pdfplumber
+
+def extract_pdf_text(filepath):
+
+    text = ""
+
+    with pdfplumber.open(filepath) as pdf:
+
+        for page in pdf.pages:
+
+            page_text = page.extract_text()
+
+            if page_text:
+                text += page_text + "\n"
+
+    return text
